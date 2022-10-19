12f00c7a486f423aac8cfc2b77214038076ffe93
enesmutlu3d <104431675+enesmutlu3d@users.noreply.github.com>
2022-10-19 11:49:17 +0300

    Add: Character Variants

---
+++ Assets/_Main/Scripts/Character_Scripts/AiMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private Animator _animator;
+}
