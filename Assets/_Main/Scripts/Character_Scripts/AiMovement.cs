91743c2162bc3194d6e12d5056a747dd915b36cc
enesmutlu3d <104431675+enesmutlu3d@users.noreply.github.com>
2022-10-19 14:14:22 +0300

    Add: First Level Demo

--- Assets/_Main/Scripts/Character_Scripts/AiMovement.cs
+++ Assets/_Main/Scripts/Character_Scripts/AiMovement.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float movementSpeed;
     [SerializeField] private Animator _animator;
+    [SerializeField] private NavMeshAgent _agent;
+    
+    
 }
